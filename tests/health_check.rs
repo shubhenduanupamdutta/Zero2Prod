a993a56975eb9e866eb6a02579acba1c67f4beb7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-17 14:09:13 +0530

    Polishing test system
    test app runs in available port, where requests are also sent

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,18 +1,24 @@
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
 }
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
