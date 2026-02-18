244b052953c3573609e6eb45784ac756d4cfcb8f
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-18 21:32:22 +0530

    Loading configuration from yaml

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,27 @@
+use config::{Config, ConfigError, File, FileFormat};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    // Initialize our configuration reader
+    let settings = Config::builder()
+        .add_source(File::new("configuration.yaml", FileFormat::Yaml))
+        .build()?;
+
+    // Try to convert the configuration values it read into our Settings type
+    settings.try_deserialize::<Settings>()
+}
