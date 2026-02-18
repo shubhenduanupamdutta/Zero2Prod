1fd79440aee83ce805c87586fe39c95352234dd7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-18 21:10:23 +0530

    Refactoring for extensibility

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,12 @@
+use actix_web::{HttpResponse, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct FormData {
+    name: String,
+    email: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
+use zero2prod::*;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{port}")
