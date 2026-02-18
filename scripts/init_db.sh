e2b727794c72cad0c171b0e72da241bed0a0ea66
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-18 20:22:30 +0530

    Full database creation script with health check and sqlx checks

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,14 @@
 set -x # Print commands and their arguments as they are executed.
 set -eo pipefail # Exit immediately if a command exits with a non-zero status, and prevent errors in a pipeline from being masked.
 
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 # Check if custom parameter has been set, otherwise use default values
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${POSTGRES_USER:=postgres}"
@@ -35,9 +43,13 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 # Create the application user
-CREATE_QUERY="CREATE ROLE $APP_USER WITH PASSWORD '$APP_USER_PWD';"
+CREATE_QUERY="CREATE USER $APP_USER WITH PASSWORD '$APP_USER_PWD';"
 docker exec -it "$CONTAINER_NAME" psql --username "$SUPERUSER" --command "$CREATE_QUERY"
 
 # Grant create db privileges to the application user
-GRANT_QUERY="ALTER ROLE $APP_USER CREATEDB;"
+GRANT_QUERY="ALTER USER $APP_USER CREATEDB;"
 docker exec -it "$CONTAINER_NAME" psql --username "$SUPERUSER" --command "$GRANT_QUERY"
+
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
+export DATABASE_URL
+sqlx database create
