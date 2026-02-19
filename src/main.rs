0628d6fdcf47d76fb511fc1b0a14128e35942fa4
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-19 12:22:48 +0530

    Monitoring app with Logger middleware and terminal output using env-logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
