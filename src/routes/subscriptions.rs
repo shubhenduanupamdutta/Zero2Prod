bbcd917f60efcb1bafdd554c04c56f66d85eaca3
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-20 08:56:07 +0530

    Adding tracing subscriber and formatting layer to the application

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::{Instrument, error, info, info_span, subscriber};
+use tracing::{Instrument, error, info, info_span};
 use uuid::Uuid;
 
 #[allow(dead_code)]
