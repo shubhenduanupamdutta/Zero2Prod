6693ebc3e7c6ec6b0e122fd02af1ac46f40f0630
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-20 09:03:07 +0530

    Configuring actix with tracing logger
    to get tracing compatible data and Initializing
    log tracer for converting log to trace compatible

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
     let subscriber = Registry::default()
