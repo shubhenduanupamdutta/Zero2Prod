592e256d312ed695fd007262dc6e1a9b4efba8cd
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-02-27 20:16:52 +0530

    Using TryInto trait and parsing NewSubscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,15 +2,28 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::error;
+use tracing::{error, subscriber};
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(Deserialize)]
 pub struct FormData {
     name: String,
     email: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(NewSubscriber {
+            name,
+            email,
+        })
+    }
 }
 
 #[tracing::instrument(name="Adding a new subscriber", skip(form, pool),
@@ -19,16 +32,9 @@ pub struct FormData {
     subscriber_name=%form.name
 ))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    // `web::Form` is a wrapper around the `FormData`
-    // `form.0` gives us access to the inner `FormData` instance
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
@@ -51,7 +57,7 @@ async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
