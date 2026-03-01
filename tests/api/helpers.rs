e38cd401f52b92d15ea66787af40efb95e470fe0
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-01 18:42:20 +0530

    Refactoring tests to make tests modular
    It also increases test discovery
    Putting tests inside a folder with main.rs (api) makes sure that
    one binary is created for all the tests in that folder. Also this
    scopes helper function to that folder.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,16 +1,17 @@
+use std::{net::TcpListener, sync::LazyLock};
+
 use secrecy::SecretString;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::{net::TcpListener, sync::LazyLock};
 use uuid::Uuid;
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
     email_client::EmailClient,
+    startup,
     telemetry::{get_subscriber, init_subscriber},
-    *,
 };
 
-// Ensuring tracing stack is only initialized once using LazyLock
-
+// Ensure that the `tracing` stack is only initialized once when the first test is run, and is not
+// re-initialized for each test
 static TRACING: LazyLock<()> = LazyLock::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
@@ -28,12 +29,12 @@ static TRACING: LazyLock<()> = LazyLock::new(|| {
     };
 });
 
-struct TestApp {
+pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
-async fn spawn_app() -> TestApp {
+pub async fn spawn_app() -> TestApp {
     LazyLock::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let port = listener.local_addr().unwrap().port();
@@ -99,115 +100,4 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to run database migrations.");
 
     connection_pool
-}
-
-#[tokio::test]
-async fn health_check_works() {
-    // Arrange
-    let app_data = spawn_app().await;
-
-    let client = reqwest::Client::new();
-
-    // Act
-    let response = client
-        .get(format!("{}/health_check", &app_data.address))
-        .send()
-        .await
-        .expect("Failed to execute request");
-
-    // Assert
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-#[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
-    let app_data = spawn_app().await;
-
-    let client = reqwest::Client::new();
-
-    // Act
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let response = client
-        .post(format!("{}/subscriptions", &app_data.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
-
-    // Assert
-    assert!(response.status().is_success());
-
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&app_data.db_pool)
-        .await
-        .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
-    assert_eq!(saved.name, "le guin");
-}
-
-#[tokio::test]
-async fn subscribe_returns_a_400_when_data_is_missing() {
-    // Arrange
-    let app_data = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    let test_cases = vec![
-        ("name=le%20guin", "missing the email"),
-        ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email"),
-    ];
-
-    for (invalid_body, error_message) in test_cases {
-        // Act
-        let response = client
-            .post(format!("{}/subscriptions", &app_data.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request");
-
-        // Assert
-        assert_eq!(
-            response.status().as_u16(),
-            400,
-            "The API did not fail with 400 Bad Request when the payload was {}",
-            error_message
-        );
-    }
-}
-
-#[tokio::test]
-async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
-    // Arrange
-    let app = spawn_app().await;
-    let client = reqwest::Client::new();
-    let test_cases = vec![
-        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
-        ("name=Ursula&email=", "empty email"),
-        ("name=Ursula&email=definitely_not_an_email", "invalid email"),
-    ];
-
-    for (body, description) in test_cases {
-        // Act
-        let response = client
-            .post(format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request");
-
-        // Assert
-        assert_eq!(
-            response.status().as_u16(),
-            400,
-            "The API did not return 400 Bad Request when the payload was {}",
-            description
-        );
-    }
 }
