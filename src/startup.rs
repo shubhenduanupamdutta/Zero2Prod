e38fe9a25b0e548d05ca828fc9290092d27bacf7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-04 12:20:45 +0530

    Basic confirm endpoint which always returns OK

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::{
     email_client::EmailClient,
-    routes::{health_check, subscribe},
+    routes::{confirm, health_check, subscribe},
 };
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::{postgres::PgPoolOptions, PgPool};
@@ -71,6 +71,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(pool.clone())
             .app_data(email_client.clone())
     })
