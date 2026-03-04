a57ca74745270b6a2778611e0551f7f8a5b28317
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-04 13:25:58 +0530

    TDD Test - clicking on confirmation link confirms a subscriber

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -46,3 +46,41 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
+
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+
+    let ConfirmationLinks {
+        link,
+    } = app.get_confirmation_links(email_request);
+
+    // Act
+    reqwest::get(link)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "confirmed");
+}
