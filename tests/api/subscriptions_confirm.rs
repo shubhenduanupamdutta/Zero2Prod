783c8f9ff83ffaa933d2cf27ddf01f0ae6a27922
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-04 12:32:45 +0530

    TDD Test - The link returned by subscribe returns a valid 200 if called

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,9 @@
 use crate::helpers::spawn_app;
+use reqwest::Url;
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
 
 #[tokio::test]
 async fn confirmations_without_token_are_rejected_with_a_400() {
@@ -12,4 +17,51 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 
     // Assert
     assert_eq!(400, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    // Extract the link from one of the request fields
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(
+            links.len(),
+            1,
+            "Expected exactly one link in the email, but found {}",
+            links.len()
+        );
+        links[0].as_str().to_owned()
+    };
+    let raw_confirmation_link = &get_link(&body["html_body"].as_str().unwrap());
+    let confirmation_link =
+        Url::parse(raw_confirmation_link).expect("Failed to parse the confirmation link as a URL.");
+
+    // Let's make sure we don't call random API on the web
+    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+
+    // Act
+    let response = reqwest::get(confirmation_link)
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
 }
