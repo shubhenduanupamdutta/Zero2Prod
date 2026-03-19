d7768da66f9f3be891cb008be942ec546fdd17d6
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-19 15:21:26 +0530

    Enable reqwest cookies and use cookie API in tests

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,7 +1,4 @@
-use std::collections::HashSet;
-
 use serde_json::json;
-use wiremock::http::HeaderValue;
 
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
@@ -18,11 +15,10 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
 
     // Assert
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
-    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication Failed").unwrap()));
+    let flash_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == "_flash")
+        .expect("No _flash cookie was found in the response");
+    assert_eq!(flash_cookie.value(), "Authentication failed");
     assert_is_redirect_to(&response, "/login");
 }
