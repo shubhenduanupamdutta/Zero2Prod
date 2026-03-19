37e2e23d2d5b858d72db9bf099d1075b26637ec5
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-19 16:33:39 +0530

    Use actix_web_flash_messages for login errors
    Replace manual _flash cookie with FlashMessage::error sent on auth
    failure

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,5 @@
 use actix_web::{HttpResponse, cookie::Cookie, error::InternalError, http::header::LOCATION, web};
+use actix_web_flash_messages::FlashMessage;
 use secrecy::SecretString;
 use serde::Deserialize;
 use sqlx::PgPool;
@@ -54,9 +55,9 @@ pub async fn login(
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
+            FlashMessage::error(e.to_string()).send();
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
-                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
             Err(InternalError::from_response(e, response))
         },
