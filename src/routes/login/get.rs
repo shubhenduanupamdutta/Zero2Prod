63ecbe8ab38cac0f928c559501d0b6549426f853
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-19 16:11:04 +0530

    Remove HMAC error verification and deps
    Simplify login_form by removing QueryParams and HmacSecret logic and
    replace error handling with a TODO placeholder. Remove hex, hmac and
    sha2
    from Cargo.toml

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,49 +1,9 @@
-use actix_web::{HttpResponse, http::header::ContentType, web};
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
+use actix_web::{HttpResponse, http::header::ContentType};
 
-use crate::startup::HmacSecret;
 
-#[derive(serde::Deserialize)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
+pub async fn login_form() -> HttpResponse {
+    let error_html = todo!();
 
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
-        let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())?;
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-        Ok(self.error)
-    }
-}
-
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
-        None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error_message) => {
-                format!(
-                    r#"<p style="color: red;">{}</p>"#,
-                    htmlescape::encode_minimal(&error_message)
-                )
-            },
-            Err(e) => {
-                tracing::warn!(
-                    error.message = %e,
-                    error.cause_chain = ?e,
-                    "Failed to verify the HMAC tag for the error message"
-                );
-                "".into()
-            },
-        },
-    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
