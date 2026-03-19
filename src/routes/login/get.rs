234c03caebb04c219724d3976ab4f0c76339783f
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-19 16:13:27 +0530

    Display flash message from cookie in login form
    Accept HttpRequest in login_form and render the _flash cookie's
    value into an italicized paragraph when present

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,11 @@
-use actix_web::{HttpResponse, http::header::ContentType};
+use actix_web::{HttpRequest, HttpResponse, http::header::ContentType};
 
 
-pub async fn login_form() -> HttpResponse {
-    let error_html = todo!();
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
+        None => "".into(),
+        Some(cookie) => format!(r#"<p><i>{}</i></p>"#, cookie.value()),
+    };
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
