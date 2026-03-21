1663024fa30b68a32156c686a4e72573ad9ff8f7
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-21 15:37:03 +0530

    Install rustls aws-lc-rs crypto provider as default option

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
+
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
