902c05ad027289913ca180cb8a474013ae9f7fcf
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-03-27 19:41:22 +0530

    Add idempotency test for newsletter creation

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -138,3 +138,45 @@ async fn create_confirmed_subscriber(app: &TestApp) {
         .error_for_status()
         .unwrap();
 }
+
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    // Arrange
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act - Part 1 - Submit newsletter form
+    let newsletter_request_body = json!({
+        "title": "Newsletter title",
+        "content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string(),
+    });
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    // Assert - Part 1 - Check that we are redirected back to the newsletter form
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 2 - Follow the redirect
+    let html_page = app.get_publish_newsletter_html().await;
+    // Assert - Part 2 - Newsletter has been published
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Act - Part 3 - Submit the same newsletter form **again**
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    // Assert - Part 3 - Check that we are redirected back to the newsletter form
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 4 - Follow the redirect
+    let html_page = app.get_publish_newsletter_html().await;
+    // Assert - Part 4 - Newsletter has been published only once
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Mock verifies on Drop that we have send the newsletter email **once**.
+}
